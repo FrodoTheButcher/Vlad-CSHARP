d3e34db7c98140d915cb23b8d4436830ff06746a
Fodor Robert <122697962+FrodoTheButcher@users.noreply.github.com>
2025-10-28 18:43:01 +0200

    afisari-citiri

---
+++ READING-DATA/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace READING_DATA
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+       mesaj = input()
+       print(mesaj)
+       */
+      string mesaj = Console.ReadLine();
+      Console.WriteLine(mesaj);
+
+
+      /*
+       x = int(input("Introduceti primul numar"))
+       y = int(input("Introduceti al doilea numar"))
+       sum = x+y
+       print(sum)
+       */
+
+      Console.WriteLine("Introduceti primul numar");
+      int x = int.Parse(Console.ReadLine());
+      Console.WriteLine("Introduceti al doilea numar");
+      int y = int.Parse(Console.ReadLine());
+      int sum = x + y;
+      Console.WriteLine(sum);
+    }
+  }
+}
