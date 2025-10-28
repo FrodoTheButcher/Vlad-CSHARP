0520ef1d651e68a7d917ad7aa3d299396fc0558e
Fodor Robert <122697962+FrodoTheButcher@users.noreply.github.com>
2025-10-28 18:37:23 +0200

    afisari

---
+++ PRINTING-DATA/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PRINTING_DATA
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //python->print("Hello world!");
+      Console.WriteLine("Hello world!");
+
+
+      int a = 5;
+      int b = 6;
+      int suma = a + b;
+      /*
+       python:
+      a=5
+      b=6
+      suma=a+b
+      print("Suma lui"+a+" si "+b+" este " + suma);
+       */
+      Console.WriteLine("Suma lui "+a+" si "+ b + " este "+suma);//INEFICIENT
+      //SAU
+      Console.WriteLine($"Suma lui {a} si {b} este {suma}");//EFICIENT 
+     }
+  }
+}
+
+/*
+ class Console{
+
+  print(),
+  readData(),
+  validate()
+}
+
+ */
